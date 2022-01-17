15fa95449253bf0dea786ff72cf68496b25f325d
Ryu-5973 <l15218537285@outlook.com>
2022-01-17 15:57:47 +0800

    Jan 17-allocator.h, construct.h

---
+++ MySTL/allocator.h
@@ -0,0 +1,102 @@
+﻿#ifndef MYTINYSTL_ALLOCATOR_H_
+#define MYTINYSTL_ALLOCATOR_H_
+
+// 这个头文件包含一个模板类 allocator，用于管理内存的分配、释放，对象的构造、析构
+
+#include "construct.h"
+#include "util.h"
+
+namespace mystl
+{
+
+// 模板类：allocator
+// 模板函数代表数据类型
+template <typename T>
+class allocator
+{
+public:
+	typedef T            value_type;
+	typedef T*           pointer;
+	typedef const T*     const_pointer;
+	typedef T&           reference;
+	typedef const T&     const_reference;
+	typedef size_t       size_type;
+	typedef ptrdiff_t    difference_type;
+
+public:
+	static T* allocate(size_type n = 1);	// 形参默认参数
+
+	static void deallocate(T* ptr, size_type n = 0);	// n无作用，默认形参重载出两个接口
+
+	static void construct(T* ptr);
+	static void construct(T* ptr, const T& value);
+	static void construct(T* ptr, T&& value);
+
+	template <typename... Args>
+	static void construct(T* ptr, Args&& ...args);
+
+	static void destroy(T* ptr);
+  	static void destroy(T* first, T* last);
+};
+
+template <typename T>
+T* allocator<T>::allocate(size_type n)
+{
+	if (n == 0) {
+	    return nullptr;
+	}
+  	// 通过new申请所需空间并用static_cast转换为所需类型
+  	return static_cast<T*>(::operator new(n * sizeof(T)));
+}
+
+
+template <typename T>
+void allocator<T>::deallocate(T* ptr, size_type n /*size*/)
+{
+  	if (ptr == nullptr) {
+    	return;
+   	}
+  	// 通过delete释放指针
+  	::operator delete(ptr);
+}
+
+template <typename T>
+void allocator<T>::construct(T* ptr)
+{
+  	mystl::construct(ptr);
+}
+
+template <typename T>
+void allocator<T>::construct(T* ptr, const T& value)
+{
+  	mystl::construct(ptr, value);
+}
+
+template <typename T>
+void allocator<T>::construct(T* ptr, T&& value)
+{
+  	mystl::construct(ptr, mystl::move(value));
+}
+
+template <typename T>
+template <typename... Args>
+void allocator<T>::construct(T* ptr, Args&&... args)
+{
+  	mystl::construct(ptr, mystl::forward<Args>(args)...);
+}
+
+template <typename T>
+void allocator<T>::destroy(T* ptr)
+{
+  	mystl::destroy(ptr);
+}
+
+template <typename T>
+void allocator<T>::destroy(T* first, T* last)
+{
+  	mystl::destroy(first, last);
+}
+
+} // namespace mystl
+#endif // !MYTINYSTL_ALLOCATOR_H_
+
---
+++ MySTL/construct.h
@@ -0,0 +1,84 @@
+﻿#ifndef MYTINYSTL_CONSTRUCT_H_
+#define MYTINYSTL_CONSTRUCT_H_
+
+// 这个头文件包含两个函数 construct，destroy
+// construct : 负责对象的构造
+// destroy   : 负责对象的析构
+
+#include <new>
+
+#include "type_traits.h"
+#include "iterator.h"
+
+#ifdef _MSC_VER
+#pragma warning(push)
+#pragma warning(disable : 4100)  // unused parameter
+#endif // _MSC_VER
+
+namespace mystl
+{
+
+// construct 构造对象
+
+template <typename Ty>
+void construct(Ty* ptr)
+{
+  	::new ((void*)ptr) Ty();
+}
+
+template <typename Ty1, typename Ty2>
+void construct(Ty1* ptr, const Ty2& value)
+{
+  	::new ((void*)ptr) Ty1(value);
+}
+
+template <typename Ty, typename... Args>
+void construct(Ty* ptr, Args&&... args)
+{
+  	::new ((void*)ptr) Ty(mystl::forward<Args>(args)...);
+}
+
+// destroy 将对象析构
+
+template <typename Ty>
+void destroy_one(Ty*, std::true_type) {}
+
+template <typename Ty>
+void destroy_one(Ty* pointer, std::false_type)
+{
+	if (pointer != nullptr) {
+    	pointer->~Ty();
+  	}
+}
+
+template <typename ForwardIter>
+void destroy_cat(ForwardIter , ForwardIter , std::true_type) {}
+
+template <typename ForwardIter>
+void destroy_cat(ForwardIter first, ForwardIter last, std::false_type)
+{
+  	for (; first != last; ++first) {
+    	destroy(&*first);
+    }
+}
+
+template <typename Ty>
+void destroy(Ty* pointer)
+{
+  	destroy_one(pointer, std::is_trivially_destructible<Ty>{});
+}
+
+template <typename ForwardIter>
+void destroy(ForwardIter first, ForwardIter last)
+{
+  	destroy_cat(first, last, std::is_trivially_destructible<typename iterator_traits<ForwardIter>::value_type>{});
+}
+
+} // namespace mystl
+
+#ifdef _MSC_VER
+#pragma warning(pop)
+#endif // _MSC_VER
+
+#endif // !MYTINYSTL_CONSTRUCT_H_
+
